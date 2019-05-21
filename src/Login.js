7e2b8516d876c407f6cdf43f783f4effe2571eaa
alepontes <alexandrelopespontes@gmail.com>
2019-05-21 19:23:13 -0300

    Primeiro Commit

---
+++ src/Login.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export default class Login extends React.Component {
+
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props)
+    }
+
+
+    render() {
+        return (
+            <div>
+                <p>Login Screen</p>
+            </div>
+        );
+    }
+
+}
